7c2e36f718c672431c58c35e6cd71389a1928f8e
José Alejandro Valduz Contreras <jose.valduz@unet.edu.ve>
2023-03-06 00:39:19 -0400

    boton Carrito de compras
    Se incorporo este boton con la opcion modal para visualizar la compra actual

--- productos.js
+++ productos.js
@@ -32,3 +32,16 @@ function choice_photo(){
     cho=document.getElementById('tipos').value
     alert(cho)
 }
+
+// POP-UP MODAL
+// const open = document.getElementById('open');
+// const modal_container = document.getElementById('modal_container');
+// const close = document.getElementById('close');
+// open.addEventListener('click', () =>{
+//     modal_container.classList.add('show');
+//     alert('prueba')
+// });
+
+// close.addEventListener('click', () =>{
+//     modal_container.classList.remove('show');
+// });
